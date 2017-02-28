495e8ac635941a4ce342561636da361a40467277
TheAlgorithm <eric@wearereasonablepeople.com>
2017-02-28 16:51:23 +0100

    Updated readme and fixed linting

--- generator/service/temp.js
+++ generator/service/temp.js
@@ -2,6 +2,7 @@ import angular from 'angular';
 
 const <%= name %>Service = function() {
   'ngInject';
+
 };
 
 angular.module('<%= APP %>.<%= name %>', []).service('<%= name %>', <%= name %>Service);
